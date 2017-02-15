cdc416c88dfc4d55e1fde6393a7bde44e58959d9
hadley <h.wickham@gmail.com>
2017-02-15 15:08:44 -0600

    Teach s3_dispatch about group and intenral Generics
    Fixes #3. Fixes #2.

--- R/dispatch.R
+++ R/dispatch.R
@@ -8,16 +8,71 @@
 #' s3_dispatch(print(x))
 #' s3_dispatch(is.numeric(x))
 #' s3_dispatch(as.Date(x))
+#' s3_dispatch(sum(x))
+#'
+#' # Internal vs. regular generic
+#' x1 <- 1
+#' x2 <- structure(2, class = "numeric")
+#'
+#' my_length <- function(x) UseMethod("my_length")
+#' s3_dispatch(my_length(x1))
+#' s3_dispatch(my_length(x2))
+#'
+#' s3_dispatch(length(x1))
+#' s3_dispatch(length(x2))
 s3_dispatch <- function(call, env = parent.frame()) {
   call <- substitute(call)
-
   generic <- as.character(call[[1]])
   x <- eval(call[[2]], env)
 
-  class <- if (is.object(x)) class(x) else implicit_class(x)
+  if (is.object(x)) {
+    class <- class(x)
+  } else {
+    class <- implicit_class(x)
+  }
+
   methods <- paste0(generic, ".", c(class, "default"))
 
+  # internal generics will always resolve to something
+  # currently showing with generic name
+  if (is_internal_generic(generic)) {
+    if (is.object(x)) {
+      methods <- c(methods, generic)
+    } else {
+      methods <- generic
+    }
+  }
+
+  # Add group generic if necssary
+  group <- find_group(generic)
+  if (!is.null(group)) {
+    group_methods <- paste0(group, ".", class)
+    methods <- c(methods, group_methods)
+  }
+
   exists <- vapply(methods, exists, logical(1), envir = env)
-  cat(paste0(ifelse(exists, "*", " "), " ", methods,
-    collapse = "\n"), "\n", sep = "")
+
+  new_s3_scalar(
+    method = methods,
+    exists = exists,
+    class = "method_table"
+  )
+}
+
+#' @export
+print.method_table <- function(x, ...) {
+  bullets <- paste0(ifelse(x$exists, "*", " "), " ", x$method, "\n", collapse = "")
+  cat(bullets, sep = "")
+
+  invisible(x)
+}
+
+find_group <- function(generic) {
+  g <- group_generics()
+  g_table <- stats::setNames(rep(names(g), lengths(g)), unlist(g))
+
+  if (!generic %in% names(g_table))
+    return()
+
+  g_table[[generic]]
 }
